bb2880721e97a8cd83e839966822c8ac978d97ee
AndyXie <andrewcrab.china@gamil.com>
2017-05-19 21:21:29 -0700

    CardWithImage for Products

--- src/components/card/CardWithImage.js
+++ src/components/card/CardWithImage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, ScrollView, StyleSheet, Platform } from 'react-native';
 
-import { Text, Button, Icon, SocialIcon, Card } from 'react-native-elements';
+import { Text, Button, Icon, SocialIcon, Card, Divider } from 'react-native-elements';
 
 import colors from 'HSColors';
 import socialColors from 'HSSocialColors';
@@ -29,6 +29,156 @@ export default class CardWithImage extends Component {
           buttonStyle={{borderRadius: 0, marginLeft: -15, marginRight: -15, marginBottom: 0}}
           title='VIEW NOW' />
         </Card>
+        <Card>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10}}>Recommended:</Text>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <Divider style={{backgroundColor: "#ccc", marginTop: 15}} />
+          <Text style={{marginTop: 15, color: "#888", fontSize: 15, alignSelf: 'center'}}>Click to Update</Text>
+        </Card>
+
+        <Card>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10}}>Recommended:</Text>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <Divider style={{backgroundColor: "#ccc", marginTop: 15}} />
+          <Text style={{marginTop: 15, color: "#888", fontSize: 15, alignSelf: 'center'}}>Click to Update</Text>
+        </Card>
+
+        <Card>
+          <Text style={{fontWeight: 'bold', fontSize: 20, marginBottom: 10}}>Recommended:</Text>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <View style={[styles.socialRow]}>
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+
+            <Card
+              containerStyle={{margin: 5, flex: 1}}
+              imageStyle={{height: 90}}
+              image={{url: 'https://leaderchat.files.wordpress.com/2016/05/leaderchat_podcast-200px.png?w=240'}}>
+              <Text>Product A</Text>
+            </Card>
+          </View>
+          <Divider style={{backgroundColor: "#ccc", marginTop: 15}} />
+          <Text style={{marginTop: 15, color: "#888", fontSize: 15, alignSelf: 'center'}}>Click to Update</Text>
+        </Card>
+
       </ScrollView>
     )
   }
